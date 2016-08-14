3c9bdc24cde13a1e4a5e4ffc12e554a39fbb468d
ni xian <nxian@CNnxian.local>
2016-08-14 17:23:26 +0800

    add the second third and forth homework

---
+++ ruby4.rb
@@ -0,0 +1,22 @@
+
+def getword(str, c)
+  i=0
+  leng=c
+  strlength=str.split.length
+
+  result = ""
+  until str.split[i].nil? do
+    if (leng-str.split[i].length-1) >= 0
+      result += str.split[i]
+      result += " "
+      leng=leng-str.split[i].length-1
+      i=i+1
+    else
+      result += "\n"
+      leng=c
+    end
+  end
+  result
+end
+
+print getword("qewe rweg we aef akvw erge rg", 15)
