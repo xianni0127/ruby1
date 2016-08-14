181a1b6de96c6ad1b249a48bfab9a3c9f0eb6847
xianni0127 <xianni0127@sina.com>
2016-08-14 18:01:10 +0800

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,22 +1,15 @@
 def putssomething (fuz)
+  result = ""
   fuz.each  do |fuz_data|
-    if fuz_data % 3 ==0 && fuz_data % 5 ==0
-      puts "FizzBuzz"
-    elsif fuz_data % 3  ==0  or fuz_data.to_s.include?"3"
-      puts "Fizz"
-    elsif fuz_data % 5 ==0  or fuz_data.to_s.include?"5"
-      puts "Buzz"
+    if fuz_data.to_i % 3 ==0 && fuz_data.to_i % 5 ==0
+        result += "FizzBuzz" + ' '
+    elsif fuz_data.to_i % 3  ==0  or fuz_data.to_s.include?"3"
+        result +=  "Fizz" + ' '
+    elsif fuz_data.to_i % 5 ==0  or fuz_data.to_s.include?"5"
+        result += "Buzz" + ' '
     elsif fuz_data.is_a?(Numeric)
-      puts fuz_data
+      result += fuz_data.to_s + ' '
     end
   end
+  return result
  end
-
- fuz= Array(1..100)
- putssomething (fuz)
-
-fuz1=Array[-1,-2,-3]
-putssomething (fuz1)
-
-fuz2=Array[1,2,3,4,5,'q','w','e']
-putssomething(fuze2)
