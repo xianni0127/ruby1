88b793befee8c46af7a69fc391f15db25d2a397a
xianni0127 <xianni0127@sina.com>
2016-08-14 17:29:17 +0800

    Create test_ruby4.rb

---
+++ test_ruby4.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require './ruby4'
+
+class TestRuby4 <Minitest::Test
+  def test_ruby4
+    assert_equal "qewe rweg we \naef akvw erge \nrg ", getword("qewe rweg we aef akvw erge rg", 15)
+  end
+end
